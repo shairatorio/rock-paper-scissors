a9a0df2345b5e582e07b7219c765e746a9c8ed54
shairatorio <46521106+shairatorio+@users.noreply.github.com>
2023-02-01 00:45:32 +0800

    Added DOM Elements

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 
+let playerCount = 0;
+let computerCount = 0;
+
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
@@ -14,20 +17,45 @@ function playRound(playerSelection, computerSelection) {
         "paper": "rock"
     };
 
+    const result = document.getElementById("result");
+    const playerScoreDisplay = document.getElementById("playerScore");
+    const computerScoreDisplay = document.getElementById("computerScore");
+
+    const tie = document.createTextNode(`It's a tie!`);
+    const win = document.createTextNode(`You Win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}`);
+    const lose = document.createTextNode(`You Lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}`);
+    const invalid = document.createTextNode(`Invalid input!`);
+    const playerScore = document.createTextNode(playerCount);
+    const computerScore = document.createTextNode(computerCount);
+
+    result.innerHTML = "";
+
     if (playerSelection === computerSelection) {
-        return "It's a tie!";
+        result.appendChild(tie);
     } else if (winObj[playerSelection] === computerSelection) {
-        return `You Win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}`;
+        result.appendChild(win);
+        playerCount++;
+        playerScoreDisplay.appendChild(playerScore);
     } else if (winObj[computerSelection] === playerSelection) {
-        return `You Lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}`;
+        result.appendChild(lose);
+        computerCount++;
+        computerScoreDisplay.appendChild(computerScore);
     } else {
-        return "Invalid input!";
+        result.appendChild(invalid);
     }
 
 }
 
+let computerSelection = getComputerChoice();
+const btnList = document.querySelectorAll('button');
+
+btnList.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        playRound(e.target.value,computerSelection);
+        });
+});
+
 function game() {
-    let computerSelection = getComputerChoice();
     let playerCount = 0;
     let computerCount = 0;
 
@@ -60,4 +88,3 @@ function game() {
         return "You Lose! Want to try again?"
     }
 }
-
